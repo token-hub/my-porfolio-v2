98e3ec92fd0d5dc252f704291e1b4029837c13d3
John Suyang <johnsuyang2118@gmail.com>
2025-11-02 02:49:17 +0800

    add and configure bootstrap and sass files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import * as bootstrap from 'bootstrap';
+import './scss/style.scss';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
