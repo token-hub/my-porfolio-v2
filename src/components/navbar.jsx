91bf783a4a540b23cc23cdbdedd183d4a4b9c331
John Suyang <johnsuyang2118@gmail.com>
2025-11-05 06:46:27 +0800

    close navbar collapsible when clicking links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,23 @@
+import { useRef } from 'react';
 import { Link } from 'react-router';
 
 function Navbar() {
+    const collapseRef = useRef();
     function handleHome() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
+        handleCollapsible(true);
     }
+    function handleCollapsible(checkNavbar = false) {
+        if (checkNavbar) {
+            const navbarCollapse = document.querySelector('.navbar-collapse');
+            if (navbarCollapse.classList.contains('show')) {
+                collapseRef.current.click();
+            }
+            return;
+        }
+        collapseRef.current.click();
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light shadow-sm fixed-top bg-white text-primary">
             <div className="container">
@@ -11,6 +25,7 @@ function Navbar() {
                     &lt;JohnSuyang /&gt;
                 </Link>
                 <button
+                    ref={collapseRef}
                     className="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
@@ -23,27 +38,27 @@ function Navbar() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav ms-auto">
-                        <li className="nav-item my-2">
+                        <li onClick={handleCollapsible} className="nav-item my-2">
                             <Link to="/#about-me" className="nav-link">
                                 About
                             </Link>
                         </li>
-                        <li className="nav-item my-2">
+                        <li onClick={handleCollapsible} className="nav-item my-2">
                             <Link to="/#projects" className="nav-link">
                                 Projects
                             </Link>
                         </li>
-                        <li className="nav-item my-2">
+                        <li onClick={handleCollapsible} className="nav-item my-2">
                             <Link to="/#skills" className="nav-link">
                                 Skills
                             </Link>
                         </li>
-                        <li className="nav-item my-2">
+                        <li onClick={handleCollapsible} className="nav-item my-2">
                             <Link to="/#certificates" className="nav-link">
                                 Certificates
                             </Link>
                         </li>
-                        <li className="nav-item my-2">
+                        <li onClick={handleCollapsible} className="nav-item my-2">
                             <Link to="/#contact-me" className="nav-link">
                                 Contact
                             </Link>
