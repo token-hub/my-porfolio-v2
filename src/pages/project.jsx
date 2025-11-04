51c8492c5e429f2b9bbc1eb171d4df24b0aa26a3
John Suyang <johnsuyang2118@gmail.com>
2025-11-05 01:54:13 +0800

    fix view project in smaller screens

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -24,18 +24,23 @@ function Project() {
                 <div className="container">
                     <div className="background" />
                     <div className="row justify-content-center">
-                        <div className="text-center mt-5">
-                            <h1 className="display-4 display-md-1 fw-semibold">{firstLetterUpperCase(displayName)}</h1>
-                            <Badges badges={badges} />
+                        <div className="col-md-8">
+                            <div className="text-center mt-5">
+                                <h1 className="display-4 display-md-1 fw-semibold">
+                                    {firstLetterUpperCase(displayName)}
+                                </h1>
+                                <Badges badges={badges} />
+                            </div>
+                            <div className="d-flex justify-content-center mt-4">
+                                <Link to={liveDemoLink} target="_blank" className="btn btn-primary">
+                                    Live Demo
+                                </Link>
+                                <Link to={sourceCodeLink} target="_blank" className="btn btn-outline-primary ms-2">
+                                    View source code
+                                </Link>
+                            </div>
                         </div>
-                        <div className="d-flex justify-content-center mt-4">
-                            <Link to={liveDemoLink} target="_blank" className="btn btn-primary">
-                                Live Demo
-                            </Link>
-                            <Link to={sourceCodeLink} target="_blank" className="btn btn-outline-primary ms-2">
-                                View source code
-                            </Link>
-                        </div>
+
                         <div className="col-md-7 my-5">
                             <picture>
                                 <source srcSet={imageWebp} />
@@ -48,7 +53,7 @@ function Project() {
             <section id="overviewFeatures" className="pt-5 bg-light">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-8 offset-2 my-4">
+                        <div className="col-md-8 offset-md-2 my-4 px-4">
                             <h2 className="fs-2 fw-bold">Project Overview</h2>
                             <div className="mt-3">
                                 {overview.length > 0 &&
