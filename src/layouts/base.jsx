eeb76e3e5a9eab4719ecdd4a18197cf975615caa
John Suyang <johnsuyang2118@gmail.com>
2025-11-05 02:23:32 +0800

    scroll to section when hash is present

--- src/layouts/base.jsx
+++ src/layouts/base.jsx
@@ -3,12 +3,17 @@ import Navbar from '../components/navbar';
 import BackToTop from '../components/backToTop';
 import { sectionAnimation } from '../lib/animations';
 import { useEffect } from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 
 function Base() {
+    const { hash } = useLocation();
     useEffect(() => {
+        if (hash) {
+            const element = document.querySelector(hash);
+            if (element) element.scrollIntoView({ behavior: 'smooth' });
+        }
         sectionAnimation('hiddenAndPushedDown');
-    }, []);
+    }, [hash]);
 
     return (
         <>
